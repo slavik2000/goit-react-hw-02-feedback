6482a041a6ce6a94bb8becc690615a33b24155bb
sяva <slavik.bieriezhnoi1995@gmail.com>
2024-08-06 21:50:58 +0300

    add and render files

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Text } from './Notification.styled';
-import { render } from '@testing-library/react';
-import { ClassNames } from '@emotion/react';
+// import { render } from '@testing-library/react';
+// import { ClassNames } from '@emotion/react';
 
 export const Notification = ({ message }) => {
   return <Text>{message}</Text>;
